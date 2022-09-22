d5f24129534ff5409e1f65907f9cae180b271f5d
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-09-22 19:03:22 +0300

    byte of python

---
+++ Byte_of_python/var.py
@@ -0,0 +1,14 @@
+# i  = 5; print(i); # ; nuzhna dlya zapisi v odnu stroku neskolko logicheskih strok
+# i = i +1
+# print(i)
+
+# s = ''' eto mnogostrochnaya stroka.
+# eto 2 ee strochka.'''
+# print(s)
+
+
+# length = 5
+# breadth = 2
+# area = length * breadth
+# print('Площадь равна', area)
+# print('Периметр равен', 2 * (length + breadth))
